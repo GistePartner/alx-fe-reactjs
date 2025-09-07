6e48aec863db7c9fd660b6e8ba1b0228f285ef05
Innocent Sydney <innocent.sydney@gmail.com>
2025-09-07 03:52:05 +0100

    smple company website using react

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ background: '#333', color: '#fff', textAlign: 'center', padding: '15px', position: 'fixed', left: 0, bottom: 0, width: '100%' }}>
+      <p>&copy; 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
