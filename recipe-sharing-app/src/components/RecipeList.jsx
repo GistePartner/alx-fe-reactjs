a69a3c8f3770b10f2abf6c195f09c2a17c315e3d
Innocent Sydney <innocent.sydney@gmail.com>
2025-09-15 20:10:39 +0100

    recipe app first commit

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,20 @@
+import useRecipeStore from '../recipestore';
+
+// RecipeList component
+const RecipeList = () => {
+  const recipes = useRecipeStore(state => state.recipes);
+
+  return (
+    <div>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
+export default RecipeList;
